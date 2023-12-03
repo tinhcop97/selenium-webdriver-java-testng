31dfb70a8a541a572100d4c8eaa26bc020ecbbcb
tinhnt01 <tinh.nt1@citigo.com.vn>
2023-12-03 23:37:45 +0700

    Update Topic_07_Textbox_Textarea_TC_02

--- selenium/webdriver/Topic_07_Textbox_Textarea.java
+++ selenium/webdriver/Topic_07_Textbox_Textarea.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -19,6 +20,8 @@ public class Topic_07_Textbox_Textarea {
     String emailAdress, firstName, lastName, password, fullName;
     Random rand = new Random();
     String employeeID = String.valueOf(rand.nextInt(99999));
+    String passportNumber = "E00007730";
+    String comment = "this is textarea\ncan enter";
 
     @BeforeClass
     public void beforeClass() {
@@ -62,7 +65,7 @@ public class Topic_07_Textbox_Textarea {
     }
 
     @Test
-    public void TC_06_validateLogin() {
+    public void TC_02_createEmployee() {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         driver.findElement(By.name("username")).sendKeys("Admin");
@@ -74,6 +77,7 @@ public class Topic_07_Textbox_Textarea {
         sleepInSecond(5);
 
 
+        //create employee
         driver.findElement(By.xpath("//label[@class='oxd-label']/parent::div/following-sibling::div//input")).sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);;
         driver.findElement(By.xpath("//label[@class='oxd-label']/parent::div/following-sibling::div//input")).sendKeys(employeeID);
         driver.findElement(By.name("firstName")).sendKeys("Tinh");
@@ -85,17 +89,50 @@ public class Topic_07_Textbox_Textarea {
         String getEmployeeID = driver.findElement(By.xpath("//label[@class='oxd-label']/parent::div/following-sibling::div//input")).getAttribute("value");
 
         driver.findElement(By.xpath("//span[@class='oxd-switch-input oxd-switch-input--active --label-right']")).click();
-        driver.findElement(By.xpath("//div[@class='oxd-form-row user-form-header']/following-sibling::div[1]//input[@class='oxd-input oxd-input--active']")).sendKeys("AutomationFC");
-        driver.findElement(By.xpath("//div[@class='oxd-grid-item oxd-grid-item--gutters user-password-cell']//input")).sendKeys("123456");
-        driver.findElement(By.xpath("//div[@class='oxd-grid-item oxd-grid-item--gutters user-password-cell']/following-sibling::div//input")).sendKeys("123456");
+        driver.findElement(By.xpath("//div[@class='oxd-form-row user-form-header']/following-sibling::div[1]//input[@class='oxd-input oxd-input--active']")).sendKeys("AutomationFC" + employeeID);
+        driver.findElement(By.xpath("//div[@class='oxd-grid-item oxd-grid-item--gutters user-password-cell']//input")).sendKeys("Password!23");
+        driver.findElement(By.xpath("//div[@class='oxd-grid-item oxd-grid-item--gutters user-password-cell']/following-sibling::div//input")).sendKeys("Password!23");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
+        //verify info
+        Assert.assertEquals(driver.findElement(By.name("firstName")).getAttribute("value"),getFirstName);
+        Assert.assertEquals(driver.findElement(By.name("lastName")).getAttribute("value"),getLastName);
+        Assert.assertEquals(driver.findElement(By.xpath("//label[@class='oxd-label']/parent::div/following-sibling::div//input")).getAttribute("value"),getEmployeeID);
 
+        driver.findElement(By.xpath("//a[text()='Immigration']")).click();
+        driver.findElement(By.xpath("//h6[text()='Assigned Immigration Records']/following-sibling::button")).click();
+        driver.findElement(By.xpath("//label[text()='Number']/parent::div/following-sibling::div//input")).sendKeys(passportNumber);
+        driver.findElement(By.xpath("//label[text()='Comments']/parent::div/following-sibling::div//textarea")).sendKeys(comment);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(By.xpath("//i[@class='oxd-icon bi-pencil-fill']")).click();
 
+        //verify info
+        sleepInSecond(3);
+        Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Number']/parent::div/following-sibling::div//input")).getAttribute("value"),passportNumber);
+        Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Comments']/parent::div/following-sibling::div//textarea")).getAttribute("value"),comment);
 
+        //logout
+        driver.findElement(By.xpath("//span[@class='oxd-userdropdown-tab']")).click();
+        driver.findElement(By.xpath("//a[text()='Logout']")).click();
 
+        driver.findElement(By.name("username")).sendKeys("AutomationFC" + employeeID);
+        driver.findElement(By.name("password")).sendKeys("Password!23");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(By.xpath("//span[text()='My Info']")).click();
 
+        sleepInSecond(3);
 
+        Assert.assertEquals(driver.findElement(By.name("firstName")).getAttribute("value"),getFirstName);
+        Assert.assertEquals(driver.findElement(By.name("lastName")).getAttribute("value"),getLastName);
+        Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div/following-sibling::div//input")).getAttribute("value"),getEmployeeID);
+
+        driver.findElement(By.xpath("//a[text()='Immigration']")).click();
+        driver.findElement(By.xpath("//i[@class='oxd-icon bi-pencil-fill']")).click();
+
+        sleepInSecond(3);
+
+        Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Number']/parent::div/following-sibling::div//input")).getAttribute("value"),passportNumber);
+        Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Comments']/parent::div/following-sibling::div//textarea")).getAttribute("value"),comment);
 
     }
 
@@ -112,6 +149,6 @@ public class Topic_07_Textbox_Textarea {
 
     @AfterClass
     public void afterClass(){
-        driver.quit();
+//        driver.quit();
     }
 }
