1985a223b26ad5971f7c23de9937d42044fc3884
tinhnt01 <tinh.nt1@citigo.com.vn>
2024-01-03 08:49:49 +0700

    Update Topic_10_Button_Radio_Checkbox

--- selenium/webdriver/Topic_10_Button_Radio_Checkbox.java
+++ selenium/webdriver/Topic_10_Button_Radio_Checkbox.java
@@ -12,8 +12,17 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Topic_10_Button_Radio_Checkbox {
     WebDriver driver;
@@ -56,53 +65,80 @@ public class Topic_10_Button_Radio_Checkbox {
 
 
     @Test
-    public void TC_02_reactJS() {
-        driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+    public void TC_getByDate() {
+        driver.get("https://rongbachkim.com/soicau.html?ngay=2024-01-03&limit=1&exactlimit=0&lon=0&nhay=1&db=1");
 
-        driver.findElement(By.cssSelector("div#root>div")).click();
+        List<WebElement> items = driver.findElements(By.xpath("//div[contains(text(),'Kết quả')]/following-sibling::a"));
+        Set<String> uniqueItems = new HashSet<>();
+        StringBuilder output = new StringBuilder();
 
-        explicitwait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//span[@class='text']")));
-
-        List<WebElement> speedDropdownItems = driver.findElements(By.xpath("//span[@class='text']"));
-        for (WebElement item : speedDropdownItems) {
+        for (WebElement item : items) {
             String itemText = item.getText();
-            System.out.println(itemText);
-
-            if (itemText.equals("Matt")) {
-                item.click();
-                Assert.assertEquals(driver.findElement(By.cssSelector("div[role='alert']")).getText(), itemText);
-                break;
-            } else {
-                System.out.println("chưa tìm thấy Matt");
+            if (!uniqueItems.contains(itemText)) {
+                uniqueItems.add(itemText);
+                output.append(itemText).append(" ");
             }
-
         }
-
+        System.out.println(output.toString().trim());
 
     }
 
     @Test
-    public void TC_03_vueJS() {
-        driver.get("https://mikerodham.github.io/vue-dropdowns/");
+    public void TC_getByDateToDate() {
+        String fromDate = "2023-12-27";
+        String toDate = "2024-01-02";
 
-        driver.findElement(By.cssSelector("li.dropdown-toggle")).click();
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Data");
 
-        explicitwait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("ul.dropdown-menu>li")));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        List<WebElement> speedDropdownItems = driver.findElements(By.cssSelector("ul.dropdown-menu>li"));
-        for (WebElement item : speedDropdownItems) {
-            String itemText = item.getText();
-            System.out.println(itemText);
+        Row headerRow = sheet.createRow(0);
+        Cell headerCellDate = headerRow.createCell(0);
+        Cell headerCellOutput = headerRow.createCell(1);
 
-            if (itemText.equals("Third Option")) {
-                item.click();
-                Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(), itemText);
-                break;
-            } else {
-                System.out.println("chưa tìm thấy Third Option");
+        headerCellDate.setCellValue("Ngày");
+        headerCellOutput.setCellValue("Kết quả");
+
+        int rowNum = 1;
+
+        LocalDate start = LocalDate.parse(fromDate, formatter);
+        LocalDate end = LocalDate.parse(toDate, formatter);
+
+        for (LocalDate date = start; !date.isAfter(end); date = date.plusDays(1)) {
+            String formattedDate = date.format(formatter);
+
+            Row row = sheet.createRow(rowNum++);
+
+            Cell dateCell = row.createCell(0);
+            dateCell.setCellValue(formattedDate);
+
+            String url = "https://rongbachkim.com/soicau.html?ngay=" + formattedDate + "&limit=1&exactlimit=0&lon=0&nhay=1&db=1";
+            driver.get(url);
+
+            List<WebElement> items = driver.findElements(By.xpath("//div[contains(text(),'Kết quả')]/following-sibling::a"));
+            Set<String> uniqueItems = new HashSet<>();
+            StringBuilder output = new StringBuilder();
+
+            for (WebElement item : items) {
+                String itemText = item.getText();
+                if (!uniqueItems.contains(itemText)) {
+                    uniqueItems.add(itemText);
+                    output.append(itemText).append(" ");
+                }
             }
 
+            Cell outputCell = row.createCell(1);
+            outputCell.setCellValue(output.toString().trim());
         }
+
+        try (FileOutputStream outputStream = new FileOutputStream("data.xlsx")) {
+            workbook.write(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
 
     }
 
